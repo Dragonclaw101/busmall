e861daf09d0424dde6b52e8a246ce83a0ee88a13
Jo-Jo Hupp <huppjoesaphean@gmail.com>
2021-05-14 08:04:21 -0700

    Progress report for bussMall assignment

--- scripts/renderImages.js
+++ scripts/renderImages.js
@@ -12,37 +12,37 @@ console.log('Hello World!');
 //Push all cards to array for rendering(?)
 
 
-let imageList = ['C/O https://placeholder.com/']
-let alt = ['Alt'];
-let descriptions = ['Descriptions'];
-let names = ['Name ex.'];
+// let imageList = ['C/O https://placeholder.com/']
+// let alt = ['Alt'];
+// let descriptions = ['Descriptions'];
+// let names = ['Name ex.'];
 
-function Card(image,productName,productDesctription,alt){
-    this.image = image;
-    this.productName = productName;
-    this.productDesctription = productDesctription;
-    this.alt = alt;
-}
+// function Card(image,productName,productDesctription,alt){
+//     this.image = image;
+//     this.productName = productName;
+//     this.productDesctription = productDesctription;
+//     this.alt = alt;
+// }
 
-function render() {
-    for (let i = 1; i<4;i++){
-        let cardDiv = document.querySelector('.images');
-        let card = document.createElement('img');
+// function render() {
+//     for (let i = 1; i<4;i++){
+//         let cardDiv = document.querySelector('.images');
+//         let card = document.createElement('img');
 
-        card.setAttribute('src', 'https://via.placeholder.com/200');
-        card.setAttribute('alt', 'https://via.placeholder.com/200');
+//         card.setAttribute('src', 'https://via.placeholder.com/200');
+//         card.setAttribute('alt', 'https://via.placeholder.com/200');
 
-        let prodName = document.createElement('h2');
-        prodName.textContent = 'Name';
+//         let prodName = document.createElement('h2');
+//         prodName.textContent = 'Name';
 
-        let prodDesc = document.createElement('p');
-        prodDesc.textContent = 'Description';
+//         let prodDesc = document.createElement('p');
+//         prodDesc.textContent = 'Description';
 
-        cardDiv.appendChild(prodName);
-        cardDiv.appendChild(card);
-        cardDiv.appendChild(prodDesc);
-    }
-}
+//         cardDiv.appendChild(prodName);
+//         cardDiv.appendChild(card);
+//         cardDiv.appendChild(prodDesc);
+//     }
+// }
 
 
-render();
+// render();
