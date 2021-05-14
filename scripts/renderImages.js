a3dad44f47578931e6d9a8854347fab4f9b2b463
Jo-Jo Hupp <huppjoesaphean@gmail.com>
2021-05-14 08:01:52 -0700

    Midway progress submission for Buss Mall project.

---
+++ scripts/renderImages.js
@@ -0,0 +1,48 @@
+'use strict';
+
+console.log('Hello World!');
+
+//TODO
+
+// Create images in the card div
+// Create a background with padding around card
+// Add name and description to card
+//Use objects + prototype and render function
+//Use asrray of images/names/descriptions
+//Push all cards to array for rendering(?)
+
+
+let imageList = ['C/O https://placeholder.com/']
+let alt = ['Alt'];
+let descriptions = ['Descriptions'];
+let names = ['Name ex.'];
+
+function Card(image,productName,productDesctription,alt){
+    this.image = image;
+    this.productName = productName;
+    this.productDesctription = productDesctription;
+    this.alt = alt;
+}
+
+function render() {
+    for (let i = 1; i<4;i++){
+        let cardDiv = document.querySelector('.images');
+        let card = document.createElement('img');
+
+        card.setAttribute('src', 'https://via.placeholder.com/200');
+        card.setAttribute('alt', 'https://via.placeholder.com/200');
+
+        let prodName = document.createElement('h2');
+        prodName.textContent = 'Name';
+
+        let prodDesc = document.createElement('p');
+        prodDesc.textContent = 'Description';
+
+        cardDiv.appendChild(prodName);
+        cardDiv.appendChild(card);
+        cardDiv.appendChild(prodDesc);
+    }
+}
+
+
+render();
